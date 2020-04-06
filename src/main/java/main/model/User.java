29151767a6b2a91d4c567c148fc8a7db65b32476
AvitomX <aleksandr0409@gmail.com>
2020-04-06 18:51:41 +0300

    created User class

---
+++ src/main/java/main/model/User.java
@@ -0,0 +1,85 @@
+package main.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "is_moderator", nullable = false)
+    private short isModerator;
+    @Column(name = "reg_time", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date regTime;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String password;
+    private String code;
+    @Lob
+    private String photo;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public short getIsModerator() {
+        return isModerator;
+    }
+
+    public void setIsModerator(short isModerator) {
+        this.isModerator = isModerator;
+    }
+
+    public Date getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(Date regTime) {
+        this.regTime = regTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
